4781e9f3c5ee6d26c8ef0c46041bee0258930d8f
ivansstef <ivansstef@gmail.com>
2025-03-31 19:31:01 +0000

    feat: add test step to CI

---
+++ GitAction/src/test/java/BasicTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BasicTest {
+    @Test
+    void alwaysPasses() {
+        assertTrue(true);
+    }
+}
